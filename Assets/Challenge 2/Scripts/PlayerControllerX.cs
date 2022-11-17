5b516718b460767fd42be22c3f45413142ce47b0
zvxo <115782098+zvxo@users.noreply.github.com>
2022-11-17 13:50:29 +0000

    1

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private bool dog = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (!dog && Input.GetKeyDown(KeyCode.Space))
+        {
+            dog = true;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Invoke("ResetDog", 1.0f);
+        }
+    }
+
+    void ResetDog()
+    {
+        dog = false;
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+
+    private float spawnIntervalMin = 2.0f;
+    private float spawnIntervalMax = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+
+        int i = Random.Range(0, ballPrefabs.Length);
+        
+        Instantiate(ballPrefabs[i], spawnPos, ballPrefabs[i].transform.rotation);
+    
+        //Random ball generated every random second(s)
+        Invoke("SpawnRandomBall", Random.Range(spawnIntervalMin, spawnIntervalMax));
+    }
+
+
+}
